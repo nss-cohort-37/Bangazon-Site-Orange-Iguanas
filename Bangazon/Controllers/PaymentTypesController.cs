c7cb0bfbc1f18bffc659acd52768a6d70ea3a61d
Willy <William.g.metcalf@gmail.com>
2020-04-27 13:25:45 -0500

    user can delete their payment types

--- Bangazon/Controllers/PaymentTypesController.cs
+++ Bangazon/Controllers/PaymentTypesController.cs
@@ -93,19 +93,29 @@ namespace Bangazon.Controllers
         }
 
         // GET: PaymentTypes/Delete/5
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            return View();
+            var paymentType = await _context.PaymentType.FirstOrDefaultAsync(pt => pt.PaymentTypeId == id);
+
+            var user = await GetUserAsync();
+
+            if (paymentType.UserId != user.Id)
+            {
+                return NotFound();
+            }
+
+            return View(paymentType);
         }
 
         // POST: PaymentTypes/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(int id, PaymentType paymentType)
         {
             try
             {
-                // TODO: Add delete logic here
+                _context.PaymentType.Remove(paymentType);
+                await _context.SaveChangesAsync();
 
                 return RedirectToAction(nameof(Index));
             }
